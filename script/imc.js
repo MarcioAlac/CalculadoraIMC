3301de9d77d925b16f2c2e439c93f52be417fc90
MarcioAlac <87020863+MarcioAlac@users.noreply.github.com>
2024-04-28 19:15:20 -0300

    v0.8
    allmost done

--- script/imc.js
+++ script/imc.js
@@ -2,25 +2,73 @@
 // neste caso a gente vai abstrair uma calculadora da vida real 
 // a gente define a class isso ak nao e HTMl nem css, é o mais proximo de uma lingagugem de programacao de vdd 
 
-class CalculadoraImc {
-
+class CalculadoraImc
+{   
     constructor()
     {
-        this.formSubmit()
+        this.request_user()
+        
+    }
+    
+    calculo(alt, pes)
+    {
+        // imc = peso/altura²
+        let calc =  (pes / alt**2) * 10000
+        // console.log(calc)
+        return calc
     }
 
-    formSubmit() {
-        document.querySelector('form').addEventListener('submit', function(event) {
-            event.preventDefault();
+    request_user()
+    { 
+        document.querySelector('form').addEventListener('submit', function (e)
+        {
+            e.preventDefault()
 
             const nome = document.querySelector('#user-name').value
-            const age = document.querySelector('#user-age').value
-            const height = document.querySelector('#user-height').value
-            const width = document.querySelector('#user-weight').value
-            console.log(nome, width, age, height);
-        });
+            const idade = document.querySelector('#user-age').value
+            
+            // height = altura 
+            const altura = document.querySelector('#user-height').value
+            const peso = document.querySelector('#user-weight').value
+            
+           
+            console.log
+            ( 
+                'nome: '  +  nome + '\n' + 
+                'idade: ' +  idade + '\n' + 
+                'altura: ' + altura + '\n' +
+                'peso: ' + peso
+            ) 
+
+            var calculo = this.calculo(altura, peso)
+            this.show()
+            this.print_result(calculo, nome, idade)
+            this.close()
+
+        }.bind(this))
+    }
+
+    show()
+    {
+        document.querySelector('.result').style.display = 'flex'
+    }
+
+    print_result(calc, name, idade)
+    {
+        document.querySelector('#result').textContent = calc.toFixed(1)
+        document.querySelector('#res-nome').textContent = name
+        document.querySelector('#res-idade').textContent = idade
+    }
+
+    close()
+    {
+        console.log(123)
+        document.querySelector('.result').addEventListener('click', function (e)
+    {
+        document.querySelector('.result').style.display = 'none'
+    })
     }
 }
 
-const IMC = new CalculadoraImc();
 
+var imc = new CalculadoraImc 
